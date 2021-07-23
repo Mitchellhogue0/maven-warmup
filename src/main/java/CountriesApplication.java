c1dea7b9257b749013db4d0a7a56a5df4ace2044
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-07-23 16:31:03 -0500

    commits maven warmup refactor

--- src/main/java/CountriesApplication.java
+++ src/main/java/CountriesApplication.java
@@ -2,5 +2,6 @@ public class CountriesApplication {
 
     public static void main(String[] args) {
         Countries country = new Countries("Unites States", "Washington, D.C.");
+        System.out.println(country);
     }
 }
